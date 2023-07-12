5e0ef39250a86be2f66c96fd3e0f11582ffcaa1c
pixodos <wi.cabrera@gmail.com>
2023-07-12 16:11:34 -0300

    TPO-CRUD-V1

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// document.getElementById("header").innerHTML = ` <nav class="navbar navbar-expand-sm
+// navbar-light bg-light">
+// <div class="container">
+// <a class="navbar-brand" href="../productos.html">CONSOLA DE ADMINISTRACION</a>
+
+// </div>
+// </nav>`
+
+document.getElementById("header").innerHTML = `
+<header>
+    <img src="img/TSLogo.png" alt="Logo" class="logo">
+</header>
+`;
+
